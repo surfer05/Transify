6d6fe8d1693158e600a22edd8fb3767e80eebe35
surfer05 <rahul05alok@gmail.com>
2023-01-14 19:38:41 +0530

    Second commit

--- router-learning/scripts/deploy.js
+++ router-learning/scripts/deploy.js
@@ -1,11 +1,12 @@
-const { ethers } = require("hardhat");
-const { GENERIC_HANDLER_GOERLI } = require("../constants");
+const {ethers}  = require("hardhat");
+const {  GOERLI_TESTNET_ADDRESS , DEST_GAS_LIMIT} = require("../constants/index.js");
 
 async function main() {
-  const genericHandlerGoerli = GENERIC_HANDLER_GOERLI;
-  const firstContract = await ethers.getContractFactory("MyContract");
+  const goerliTesnetAddress = GOERLI_TESTNET_ADDRESS;
+  const destGasLimit = DEST_GAS_LIMIT;
+  const firstContract = await ethers.getContractFactory("MyContract")
   const deployedFirstContract = await firstContract.deploy(
-    genericHandlerGoerli
+    "",goerliTesnetAddress,destGasLimit
   );
   await deployedFirstContract.deployed();
 
